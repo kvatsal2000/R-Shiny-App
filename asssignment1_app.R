1ea5ed99e2953aeedcef5094cffa6f5d77fe4833
Kumar Vatsal <kvat0001@student.monash.edu>
2022-08-15 03:10:36 +1000

    added server code

--- asssignment1_app.R
+++ asssignment1_app.R
@@ -8,6 +8,15 @@ resources <- read_csv("natural-resources.csv") %>%
 
 
 
+
+resources2 <- read_csv("natural-resources.csv") %>%
+  select(`Gas production`,`Gas consumption`,`Coal production`,`Coal consumption`,`Oil production`,`Oil consumption`,`Entity`,`Year`,`Gas reserves`,`Oil reserves`,`Coal reserves`) %>%
+  pivot_longer(cols = `Gas production`:`Oil consumption`,
+               names_to = 'type',
+               values_to = 'quantity' )
+
+
+
 ui <- fluidPage(
 
   titlePanel("Analysis of Natural Resources of Different Countries"),
@@ -15,11 +24,11 @@ ui <- fluidPage(
   h3("1.Resource production of different countries"),
 
   fluidRow(
-    selectInput("country","Select a country",choices = unique(resources$Entity)),
-    selectInput("res","Select a resource", choices = c("Oil","Natural Gas","Coal")),
+    selectInput("country","Select a country",choices = unique(resources2$Entity)),
+    selectInput("res","Select a resource", choices = c("Oil production","Gas production","Coal production")),
 
   mainPanel(
-    plotOutput("plot1")
+    plotlyOutput("plot1")
   )),
 
 
@@ -28,20 +37,45 @@ ui <- fluidPage(
   h3("2.Resource consumption of different countries"),
 
   fluidRow(
-    selectInput("country","Select a country",choices = unique(resources$Entity)),
-    selectInput("res","Select a resource", choices = c("Oil","Natural Gas","Coal")),
+    selectInput("country2","Select a country",choices = unique(resources2$Entity)),
+    selectInput("res2","Select a resource", choices = c("Oil consumption","Gas consumption","Coal consumption")),
 
   mainPanel(
-    plotOutput("plot2")
+    plotlyOutput("plot2")
   )
 
-))
+)
+)
 
 
 
 
 
 server <- function(input, output, session) {
+
+  output$plot1 <- renderPlotly({
+  rec1 <- resources2 %>%
+      filter(Entity ==  input$country) %>%
+    filter( type == input$res)
+
+   p<- ggplot(rec1,aes(x = `Year`,
+                         y= quantity)) + geom_line(stat = 'identity')
+   ggplotly(p)
+
+  })
+
+
+
+  output$plot2 <- renderPlotly({
+    rec2 <- resources2 %>%
+      filter(Entity ==  input$country2) %>%
+      filter( type == input$res2)
+
+    p<- ggplot(rec2,aes(x = `Year`,
+                        y= quantity)) + geom_line(stat = 'identity')
+    ggplotly(p)
+
+  })
 
 }
 
